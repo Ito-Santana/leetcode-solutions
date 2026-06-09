bc8029be16eb99277edf434fcac1451d00b0bd26
Ítalo Santana <156137005+Ito-Santana@users.noreply.github.com>
2026-06-09 11:46:52 -0300

    Time: 4 ms (0.94%) | Memory: 19.3 MB (57.15%) - LeetSync

---
+++ sql/basic-joins/66-plus-one/plus-one.py
@@ -0,0 +1,8 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        x_value = int("".join(map(str, digits)))
+        output_value = str(x_value+1)
+        output_list = []
+        for i in range(0, len(output_value)):
+            output_list.append(int(output_value[i]))
+        return output_list
